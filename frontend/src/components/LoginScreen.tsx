2b9890180b8f5fd04f11a54920ed535ca0333af6
awand795 <awand795@gmail.com>
2026-06-12 17:02:27 +0700

    fix: change hardcoded password

--- frontend/src/components/LoginScreen.tsx
+++ frontend/src/components/LoginScreen.tsx
@@ -11,7 +11,7 @@ export const LoginScreen: React.FC<LoginScreenProps> = ({ onLogin }) => {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if (password === "Adaulardisepatuku123456789#$#$") {
+    if (password === "Menujutakterbatasdanmelampauinya123#$#*@") {
       setError(false);
       onLogin();
     } else {
